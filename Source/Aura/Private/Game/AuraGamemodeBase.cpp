b962ef871f05963161fbb2799d373d99c5a45cfd
WIN11HOME\BRLox <BRLoxton@Gmail.com>
2023-09-21 15:01:40 +0100

    EnemyInterface

---
+++ Source/Aura/Private/Game/AuraGamemodeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright- Brian.
+
+
+#include "Game/AuraGamemodeBase.h"
+
---
+++ Source/Aura/Public/Game/AuraGamemodeBase.h
@@ -0,0 +1,17 @@
+// Copyright- Brian.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AuraGamemodeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraGamemodeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
