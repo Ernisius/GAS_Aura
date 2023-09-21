106269598195881c9e783b0fcc1b638dc078d9f5
WIN11HOME\BRLox <BRLoxton@Gmail.com>
2023-09-21 16:50:57 +0100

    EnemyInterface

--- Source/Aura/Public/AuraPlayerController.h
+++ Source/Aura/Public/AuraPlayerController.h
@@ -8,6 +8,7 @@
 
 class UInputMappingContext;
 class UInputAction;
+class IEnemyInterface;
 /**
  * 
  */
@@ -17,6 +18,7 @@ class AURA_API AAuraPlayerController : public APlayerController
 	GENERATED_BODY()
 public:
 	AAuraPlayerController();
+	virtual void PlayerTick(float DeltaTime) override;
 protected:
 
 	virtual void BeginPlay() override;
@@ -30,4 +32,9 @@ private:
 	TObjectPtr<UInputAction> MoveAction;
 
 	void Move(const struct FInputActionValue& InputActionValue);
+	void CursorTrace();
+
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
+	
 };
