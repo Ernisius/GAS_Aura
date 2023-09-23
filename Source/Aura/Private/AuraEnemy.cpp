785cbfb39c179bb46a1276bc168be43eca8968e0
WIN11HOME\BRLox <BRLoxton@Gmail.com>
2023-09-23 12:06:26 +0100

    20_construct ASC and AS

--- Source/Aura/Private/AuraEnemy.cpp
+++ Source/Aura/Private/AuraEnemy.cpp
@@ -3,11 +3,17 @@
 
 #include "AuraEnemy.h"
 
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "Aura/Aura.h"
 
 AAuraEnemy::AAuraEnemy()
 {
 	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAttributeSet>("AttributeSet");
+	
 }
 
 void AAuraEnemy::HighLightActor()
