a5a19805be60e076c45c721bd46f1abf74e6fadb
WIN11HOME\BRLox <BRLoxton@Gmail.com>
2023-09-23 10:42:10 +0100

    17_PLayerState

--- Source/Aura/Public/AuraEnemy.h
+++ Source/Aura/Public/AuraEnemy.h
@@ -16,9 +16,9 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 
 public:
+	AAuraEnemy();
 	virtual  void HighLightActor() override;
 	virtual  void UnHighLightActor() override;
 
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted= false;;
+	
 };
